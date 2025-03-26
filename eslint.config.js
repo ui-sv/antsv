57498657bdbd98d7c9b58aaaffa43736b20ec089
Boe Reh <justboereh@gmail.com>
2025-03-26 13:01:52 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,12 @@
 import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-
-/**
- *
- * @param {string} ignoreFilePath
- * @returns
- */
-function includeIgnoreFile(ignoreFilePath) {
-	if (!path.isAbsolute(ignoreFilePath)) {
-		throw new Error('The ignore file location must be an absolute path.');
-	}
-
-	const ignoreFile = fs.readFileSync(ignoreFilePath, 'utf8');
-	const lines = ignoreFile.split(/\r?\n/u);
-
-	return {
-		name: 'Imported .gitignore patterns',
-		ignores: lines
-			.map((line) => line.trim())
-			.filter((line) => line && !line.startsWith('#'))
-			.map(convertIgnorePatternToMinimatch)
-	};
-}
 
 export default ts.config(
 	includeIgnoreFile(gitignorePath),
